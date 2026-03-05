6cbd5e410a1beba3d783f2f4eab59a61d541dd33
mousty <moustapha.dev03@gmail.com>
2026-03-05 09:47:17 +0100

    [DEV] - virtual threads enabled to improve performance

---
+++ src/main/java/com/mousty00/chat_noir_api/config/TomcatConfig.java
@@ -0,0 +1,16 @@
+package com.mousty00.chat_noir_api.config;
+
+import org.springframework.boot.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class TomcatConfig {
+    
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
