9b7671179c52a6d3e4ccb54a8455b80746662749
mousty <moustapha.dev03@gmail.com>
2026-03-20 19:44:08 +0100

    [fix] - better handling render envs

--- src/main/java/com/mousty00/chat_noir_api/config/CorsConfig.java
+++ src/main/java/com/mousty00/chat_noir_api/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.mousty00.chat_noir_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins:http://localhost:3000}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://chatnoir.fun")
+                .allowedOrigins(allowedOrigins.split(","))
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
