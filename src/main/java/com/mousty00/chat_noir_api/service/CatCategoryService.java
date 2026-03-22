f8ea5e3433c6433ead0e278dab3b13f9071054a4
mousty <moustapha.dev03@gmail.com>
2026-03-22 14:32:58 +0100

    [FIX] - removed categories cache

--- src/main/java/com/mousty00/chat_noir_api/service/CatCategoryService.java
+++ src/main/java/com/mousty00/chat_noir_api/service/CatCategoryService.java
@@ -4,17 +4,13 @@ import com.mousty00.chat_noir_api.dto.api.ApiResponse;
 import com.mousty00.chat_noir_api.dto.cat.CatCategoryDTO;
 import com.mousty00.chat_noir_api.entity.CatCategory;
 import com.mousty00.chat_noir_api.exception.CatException;
-import com.mousty00.chat_noir_api.exception.ResourceNotFoundException.ResourceType;
 import com.mousty00.chat_noir_api.util.generic.GenericService;
 import com.mousty00.chat_noir_api.mapper.CatCategoryMapper;
 import com.mousty00.chat_noir_api.repository.CatCategoryRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class CatCategoryService extends GenericService<CatCategory, CatCategoryDTO, CatCategoryRepository, CatCategoryMapper> {
@@ -24,7 +20,6 @@ public class CatCategoryService extends GenericService<CatCategory, CatCategoryD
     }
 
     @Transactional
-    @Cacheable("categories")
     public ApiResponse<List<CatCategoryDTO>> getCategories() {
         try {
             List<CatCategoryDTO> categories = repo.findAll().stream().map(mapper::toDTO).toList();
@@ -33,19 +28,4 @@ public class CatCategoryService extends GenericService<CatCategory, CatCategoryD
             throw CatException.categoriesError(e);
         }
     }
-
-    @Override
-    @Transactional
-    @CacheEvict(value = "categories", allEntries = true)
-    public ApiResponse<CatCategoryDTO> saveItem(CatCategoryDTO dto) {
-        return super.saveItem(dto);
-    }
-
-    @Override
-    @Transactional
-    @CacheEvict(value = "categories", allEntries = true)
-    public ApiResponse<?> deleteItemById(UUID id) {
-        return super.deleteItemById(id);
-    }
-
 }
